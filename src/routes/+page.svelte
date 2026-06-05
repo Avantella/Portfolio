34bb03ed207639c0ad5582b96b4d24fbfb6ca412
Avantella <ggroenner@gmail.com>
2026-06-05 10:20:36 +0200

    changed sizing of snail from % to px

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,94 +1,105 @@
 <script>
-    import SeeSnail from "$lib/images/monochrome/seesnail.jpg";
-    import SeeSnail2 from "$lib/images/monochrome/seesnail2.jpg";
+  import SeeSnail from "$lib/images/monochrome/seesnail.jpg";
+  import SeeSnail2 from "$lib/images/monochrome/seesnail2.jpg";
 
-    let isRunning = true;
+  let isRunning = true;
 </script>
 
 <main>
-    <div class="container">
-        <button class="snailBtn" onclick={() => (isRunning = !isRunning)}>
-            <img
-                src={SeeSnail}
-                alt="snail"
-                class="snail1"
-                style="--play-state: {isRunning ? 'running' : 'paused'}"
-            />
-            <img
+  <div class="container">
+    <button class="snailBtn" onclick={() => (isRunning = !isRunning)}>
+      <img
+        src={SeeSnail}
+        alt="snail"
+        class="snail1"
+        style="--play-state: {isRunning ? 'running' : 'paused'}"
+      />
+      <img
                 src={SeeSnail2}
                 alt="snail"
                 class="snail2"
                 style="--play-state: {isRunning ? 'running' : 'paused'}"
             />
-        </button>
-    </div>
+    </button>
+  </div>
 </main>
 
 <style>
-    .container {
-        width: 100dvw;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        /* background-color: red; */
+  .container {
+    width: 100dvw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    /* background-color: red; */
+  }
+
+  .snailBtn {
+    border-style: none;
+    background-color: transparent;
+    height: 100%;
+    width: 50dvw;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    box-shadow: none;
+  }
+
+  .snail1 {
+    /* min-width: 30%;
+        min-height: 60%; */
+    /* max-width: 70%;
+        max-height: 70%; */
+
+    min-width: 10px;
+    min-height: 140px;
+
+    max-width: 700px;
+    max-height: 850px;
+    position: absolute;
+
+    animation: slidetoright 200s infinite;
+
+    animation-delay: 10s;
+    z-index: 1;
+    background-color: #fff;
+    animation-play-state: var(--play-state, paused);
+  }
+
+  .snail2 {
+    /* min-width: 30%;
+        min-height: 60%; */
+    /* max-width: 70%;
+        max-height: 70%; */
+
+    min-width: 10px;
+    min-height: 140px;
+
+    max-width: 700px;
+    max-height: 850px;
+
+    position: absolute;
+    z-index: 1;
+    animation: slidefromleft 200s infinite;
+    animation-play-state: var(--play-state, paused);
+    animation-delay: 10s;
+  }
+
+  @keyframes slidefromleft {
+    0% {
+      transform: translateX(-130vw);
     }
-
-    .snailBtn {
-        border-style: none;
-        background-color: transparent;
-        height: 100%;
-        width: 50dvw;
-        justify-content: center;
-        align-items: center;
-        display: flex;
-        box-shadow: none;
+    100% {
+      transform: translateX(-10);
     }
+  }
 
-    .snail1 {
-        min-width: 40%;
-        max-width: 70%;
-        min-height: 60%;
-        max-height: 70%;
-        position: absolute;
-
-        animation: slidetoright 200s infinite;
-
-        animation-delay: 10s;
-        z-index: 1;
-        background-color: #fff;
-        animation-play-state: var(--play-state, paused);
+  @keyframes slidetoright {
+    0% {
+      transform: translateX(0);
     }
-
-    .snail2 {
-        min-width: 40%;
-        max-width: 70%;
-        min-height: 60%;
-        max-height: 70%;
-
-        position: absolute;
-        z-index: 1;
-        animation: slidefromleft 200s infinite;
-        animation-play-state: var(--play-state, paused);
-        animation-delay: 10s;
+    100% {
+      transform: translateX(130vw);
     }
-
-    @keyframes slidefromleft {
-        0% {
-            transform: translateX(-130vw);
-        }
-        100% {
-            transform: translateX(-10);
-        }
-    }
-
-    @keyframes slidetoright {
-        0% {
-            transform: translateX(0);
-        }
-        100% {
-            transform: translateX(130vw);
-        }
-    }
-  
+  }
 </style>
